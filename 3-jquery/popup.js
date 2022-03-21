e9d37d43ccf604735b1abd500101b02712f9ba7a
Anna Ikoki <ikokianna@gmail.com>
2022-03-21 09:38:58 +0100

    clean out deleted files

--- 3-jquery/popup.js
+++ 3-jquery/popup.js
@@ -1,14 +1,7 @@
 // Select location value 
-// $(function(){
-//   $('#location').keyup(function(){
-  // Get the value of a form input to modify the string
-//     $('#hello').text('Hello ' + $('#location').val());
-//   })
-// })
-
-
-let inputBox = document.getElementById('location');
-
-inputBox.onkeyup = function(){
-    document.getElementById('hello').innerHTML = `Hello ${inputBox.value}`;
-}
+$(function(){
+  $('#location').keyup(function(){
+  // Get the value of a form input and modify the string
+    $('#hello').text('Hello ' + $('#location').val());
+  })
+})
--- 3-js-vs-jquery/popup.js
+++
@@ -1,8 +0,0 @@
-// Select location value 
-$(function(){
-  $('#location').keyup(function(){
-  // Get the value of a form input to modify the string
-    $('#hello').text('Hello ' + $('#location').val());
-  })
-})
-
